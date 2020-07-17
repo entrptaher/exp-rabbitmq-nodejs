cb1483b6e7d41373012d0ddddb7da856fdd39a3d
Md Abu Taher <entrptaher@gmail.com>
2020-07-17 20:16:04 +0600

    fix: queue name

--- consumer.js
+++ consumer.js
@@ -7,10 +7,13 @@ const host = process.env.AMQP_HOST;
   try {
     console.log('connecting');
     const conn = await amqp.connect(host);
+    console.log('connected');
+
+    console.log('create channel');
     const ch = await conn.createChannel();
     await ch.assertQueue(queueName, { durable: false });
-    console.log('connected');
-    
+    console.log('created channel');
+
     // Subscribe
     await ch.consume(queueName, (msg) => {
       if (msg !== null) {
